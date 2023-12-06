1d5c33c1344e3d9b053d53f86ce35d4115e83fe9
Cristina Palmisani <cristina.palmisani@outlook.it>
2023-12-06 17:22:43 +0100

    Modifiche esercizio 2 e aggiunta esercizio 3

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("----------------------- Esercizio 1 ----------------------");
 
-        Predicate<Product> isBook = product -> product.getCategory().equals("Books");
-        Predicate<Product> priceBook = product -> product.getPrice() > 100;
-        ProductFilter bookFilterPrice = products -> products.stream().filter(product -> isBook.test(product) && priceBook.test(product)).toList();
-
+        //------------------ Lista prodotti e utenti -----------------------
         Customer maria = new Customer("Maria", 3);
         Customer mario = new Customer("Mario", 2);
         Customer chiara = new Customer("Chiara", 5);
@@ -24,12 +21,22 @@ public class Main {
         Product book2 = new Product("libro2", "Books", 20);
         Product book3 = new Product("libro3", "Books", 140);
         Product book4 = new Product("libro4", "Books", 50);
+
         Product baby1 = new Product("biberon", "Baby", 30);
         Product baby2 = new Product("ghette", "Baby", 50);
         Product baby3 = new Product("ciuccio", "Baby", 20);
 
+        Product boy1 = new Product("rasoio", "Boys", 15);
+        Product boy2 = new Product("dopobarba", "Boys", 12.3);
+        Product boy3 = new Product("boxeur", "Boys", 20.60);
+
         List<Product> products = Arrays.asList(book1, book2, book3, book4);
-        System.out.println(bookFilterPrice.getProducts(products));
+        Predicate<Product> isBook = product -> product.getCategory().equals("Books");
+        Predicate<Product> priceBook = product -> product.getPrice() > 100;
+        List<Product> bookFilterPrice =  products.stream().filter(product -> isBook.test(product) && priceBook.test(product)).toList();
+
+        System.out.println(bookFilterPrice.size());
+        bookFilterPrice.forEach(System.out::println);
 
         System.out.println("----------------------- Esercizio 2 ----------------------");
 
@@ -47,6 +54,14 @@ public class Main {
         ordersWhithBaby.forEach(System.out::println);
 
         System.out.println("----------------------- Esercizio 3 ----------------------");
+
+        List<Product> products1 = Arrays.asList(boy1, boy2, boy3, baby1, book2);
+
+        List<Product> discountedBoysProduct = products1.stream().filter(product -> product.getCategory().equals("Boys")).map(product -> new Product(product.getName(), product.getCategory(),(int)(product.getPrice()) * 0.9)).toList();
+        System.out.println(products1);
+        System.out.println(discountedBoysProduct);
+
+        System.out.println("----------------------- Esercizio 4 ----------------------");
 
     }
 }
