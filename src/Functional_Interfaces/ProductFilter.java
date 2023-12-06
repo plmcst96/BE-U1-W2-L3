d9f30b7bd237153eb8cd2af5bca7ef6e6d9c348a
Cristina Palmisani <cristina.palmisani@outlook.it>
2023-12-06 17:23:08 +0100

    Eliminazione package

--- src/Functional_Interfaces/ProductFilter.java
+++
@@ -1,9 +0,0 @@
-package Functional_Interfaces;
-
-import Shop.Product;
-
-import java.util.List;
-
-public interface ProductFilter {
-    public List<Product> getProducts(List<Product> products);
-}
